dfcf750fbb9c3f83a8cec3eed9d203adc2cd5659
Derk-R <derk.ruiter@gmail.com>
2025-05-22 15:07:42 +0200

    Create AnagramFunctions.js
    The JavaScript code for my simple anagram checker.

---
+++ AnagramFunctions.js
@@ -0,0 +1,33 @@
+function anagramChecker(word1, word2) {
+  document.getElementById('results-text').classList.add('hidden');
+  document.getElementById('results-forgot').classList.add('hidden');
+
+
+
+	if (word1.toLowerCase() === word2.toLowerCase()) {
+    document.getElementById('results-idiot').classList.remove('hidden');
+    return
+  } else if (word1.toLowerCase() !== word2.toLowerCase()) {
+      document.getElementById('results-idiot').classList.add('hidden');
+  }
+  
+  if (word1.replace(/\s+/g, '').length < 1 || word2.replace(/\s+/g, '').length < 1) {
+  	    document.getElementById('results-forgot').classList.remove('hidden');
+        return
+  }
+
+  let word1Sorted = word1.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+  let word2Sorted = word2.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+  
+  document.getElementById('results-text').classList.remove('hidden');
+
+  if (word1Sorted === word2Sorted) {
+    document.getElementById('results-are').classList.remove('hidden');
+    document.getElementById('results-arent').classList.add('hidden');
+    console.log('the words are anagrams!');
+  } else {
+    document.getElementById('results-arent').classList.remove('hidden');
+    document.getElementById('results-are').classList.add('hidden');
+    console.log('the words are not anagrams!')
+  }
+}
